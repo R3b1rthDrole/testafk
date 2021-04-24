99a1620e0d6f8e4372fa789dac524b244c2ec3e4
R3b1rthDrole <49059573+R3b1rthDrole@users.noreply.github.com>
2021-04-25 01:41:09 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+//load discord
+const Discord = require('discord.js')
+const client = new Discord.Client()
+
+//salons discord
+let channeler;
+client.on('ready', async() => {
+    channeler = await client.channels.cache.get("835254128334077952")
+    console.log(`The discord bot logged in! Username: ${client.user.username}!`)
+    if (channeler) console.log("channel trouvé")
+    let time = 0;
+    setInterval(function() {
+        console.log("tt")
+        time++;
+        channeler.send(time)
+    }, 60 * 1000)
+})
+
+//Connection discord
+client.login("ODA3MDI4MDIyNDcyNzM2Nzg4.YByBag.R_aEZqP7w-UDTVgOXQwG750jBAY")
